a968292b7aad27324e7316b6fdf5584e5226fe35
Shrey <guptashrey555@gmail.com>
2024-11-16 00:14:20 +0530

    basic check

--- app/urls.py
+++ app/urls.py
@@ -6,11 +6,9 @@ from django.urls import path
 from .views import StudentsAPI, StudentsbyIdAPI, student_summary_view
 
 urlpatterns = [
-
     path('students/',StudentsAPI.as_view() ),
     path('students/<int:student_id>/',StudentsAPI.as_view() ),
     path('student-id/<int:id>/',StudentsbyIdAPI.as_view() ),
     path('students/<int:id>/summary/', student_summary_view, name='student_summary'),
-    
-    
+       
 ]
